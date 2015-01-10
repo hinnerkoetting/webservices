237fcdccb3186707026b3833a5da6ac5c7069d8b
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-01-10 12:17:14 +0100

    added base for jws ejb server

---
+++ jws-server-ejb/src/main/java/de/oetting/webservices/server/ejb/EjbWervice.java
@@ -0,0 +1,13 @@
+package de.oetting.webservices.server.ejb;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+@WebService
+@Stateless
+public class EjbWervice {
+
+	public void foo(int i) {
+	}
+	
+}
