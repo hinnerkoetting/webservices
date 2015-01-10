cbdea73a14a3db963890c94527efb44fd8e1ffaf
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-01-10 12:49:09 +0100

    added ejb webservice

--- jws-server-ejb/src/main/java/de/oetting/webservices/server/ejb/EjbWebservice.java
+++ jws-server-ejb/src/main/java/de/oetting/webservices/server/ejb/EjbWebservice.java
@@ -3,9 +3,10 @@ package de.oetting.webservices.server.ejb;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
 
+//accessable at http://localhost:8090/jws-server-ejb-0.0.1-SNAPSHOT/EjbWebservice?wsdl (maybe port has to change)
 @WebService
 @Stateless
-public class EjbWervice {
+public class EjbWebservice {
 
 	public void foo(int i) {
 	}
