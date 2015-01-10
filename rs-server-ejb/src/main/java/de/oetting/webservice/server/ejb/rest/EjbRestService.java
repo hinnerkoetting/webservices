69b4837d3b8f3f99adb291590086807784fb265c
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-01-10 15:43:37 +0100

    ejb rest service is now accesable

--- rs-server-ejb/src/main/java/de/oetting/webservice/server/ejb/rest/EjbRestService.java
+++ rs-server-ejb/src/main/java/de/oetting/webservice/server/ejb/rest/EjbRestService.java
@@ -6,11 +6,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Application;
 
-@Path("add")
+//accessed from http://localhost:8090/jrs-server/rest/ejb/add/1/2 only via war-project
+@Path("ejb")
 @Stateless
 public class EjbRestService extends Application {
 
-	@Path("{int1}/{int2}")
+	@Path("add/{int1}/{int2}")
 	@GET
 	public String add(@PathParam("int1") int i1, @PathParam("int2") int i2) {
 		return "" + (i1 + i2);
