17a99b534c55aa5b09b0c51ec59e2ff4f3cf882b
Hinnerk Oetting <hinnerk.oetting@consileon.de>
2015-01-10 13:11:03 +0100

    added ejb as dependency of war project

--- rs-server-ejb/src/main/java/de/oetting/webservice/server/ejb/rest/EjbRestService.java
+++ rs-server-ejb/src/main/java/de/oetting/webservice/server/ejb/rest/EjbRestService.java
@@ -1,15 +1,17 @@
 package de.oetting.webservice.server.ejb.rest;
 
-import javax.ws.rs.ApplicationPath;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("ejb")
-@Path("test")
+@Path("add")
+@Stateless
 public class EjbRestService extends Application {
 
-	@Path("add/{int1}/{int2}")
+	@Path("{int1}/{int2}")
+	@GET
 	public String add(@PathParam("int1") int i1, @PathParam("int2") int i2) {
 		return "" + (i1 + i2);
 	}
